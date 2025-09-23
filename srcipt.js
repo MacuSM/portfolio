bd9baff395452afeede49b1dce5359c77af67985
Sbusiso Macu <macumog@gmail.com>
2025-09-24 00:32:23 +0200

    Changes

---
+++ srcipt.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if ($(this).scrollTop() > 550) {
+          $('.navbar').addClass('solid');
+          $('.back-to-top').addClass('visible');
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.back-to-top').removeClass('visible');
+      }
+  });
+});
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        // Optional: Add hash to URL after scroll
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+$(document).ready(function () {
+  // Skill progress bar widths
+  $('.html').css('width', '90%');
+  $('.css').css('width', '85%');
+  $('.bootstrap').css('width', '80%');
+  $('.javascript').css('width', '85%');
+  $('.python').css('width', '85%');
+  $('.django').css('width', '80%');
+  $('.react\\.js').css('width', '80%'); // Escape dot in class name
+  $('.react-native').css('width', '80%');
+  $('.nodejs').css('width', '75%');
+  $('.expressjs').css('width', '70%');
+  $('.git').css('width', '70%');
+  $('.databases').css('width', '70%');
+});
